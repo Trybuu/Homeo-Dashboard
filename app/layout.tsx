1eaa88c93ff8e0ab3ed99e61311d5f34ab0b4936
Marek Trybuła <trybu001@gmail.com>
2024-07-26 19:42:32 +0200

    Modal component, modal store

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+
+import { ModalProvider } from '@/providers/modal-provider'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,9 +19,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
