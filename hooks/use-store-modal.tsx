5d173655bf4c192f4ae5d0a7a842982426b555ba
Marek Trybuła <trybu001@gmail.com>
2024-07-29 19:28:07 +0200

    Form component

--- hooks/use-store-modal.tsx
+++ hooks/use-store-modal.tsx
@@ -1,10 +1,10 @@
 import { create } from 'zustand'
-
 interface userStoreModalStore {
   isOpen: boolean
   onOpen: () => void
   onClose: () => void
 }
+
 export const useStoreModal = create<userStoreModalStore>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
